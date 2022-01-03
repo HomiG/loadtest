6980f4a3df1a3ed01b315a4e9ec1b558a595809a
Homi <44709877+HomiG@users.noreply.github.com>
2022-01-03 21:12:53 +0200

    Loadtest Multiple Endpoints

--- loadtest.js
+++ loadtest.js
@@ -2,10 +2,10 @@ const axios = require('axios');
 // function that executes 'function' (e.g. a requester n-Times).  ?randomized delay 1-30 ms
 function runThisNTimes(functionToRun, nTimes, hasDelay = true, timeToExecuteTotalRequests) {
     let _currDelay = 0;
-
-    [...Array(nTimes).keys()].map(() => {
+    [...Array(nTimes).keys()].map((el, index) => {
         _currDelay += singleRequestDelay(nTimes, timeToExecuteTotalRequests);
         setTimeout(() => {
+            console.log({ hasSent: index, mustSend: nTimes });
             functionToRun()
         }, hasDelay ? _currDelay : 0)
     })
@@ -16,17 +16,17 @@ function runThisNTimes(functionToRun, nTimes, hasDelay = true, timeToExecuteTota
 // function that sends get request to link
 async function requestThisEndPoint(link) {
     let resp = await axios.get(link)
-    console.log(resp.data);
+    // console.log(resp.data);
 }
 
 // function to calculate single request delay based on total requests and the time that it takes to complete those requests.
 // aim is requests to be distributed harmonically in their time range.
 function singleRequestDelay(totalRequests, timeToExecuteTotalRequests) {
     let currentRequestAVGDelay = timeToExecuteTotalRequests / totalRequests;
-    // console.log(totalRequests, timeToExecuteTotalRequests, currentRequestAVGDelay);
+    console.log({ totalRequests, timeToExecuteTotalRequests, currentRequestAVGDelay });
 
     //add or remove delay of request
-    Math.floor(Math.random() * 2) === 0 ? currentRequestAVGDelay += randomDelay() : currentRequestAVGDelay -= randomDelay();
+    Math.floor(Math.random() * 2) === 0 ? currentRequestAVGDelay += randomDelay(1) : currentRequestAVGDelay -= randomDelay(1);
 
     return currentRequestAVGDelay;
 }
@@ -40,28 +40,36 @@ function randomDelay(maxDelay = 30) {
 
 
 
-function simulation({ samplingRateMs, requestPerSamplingRateMsARRAY }) {
+function simulation({ samplingRateMs, requestPerSamplingRateMsARRAY, endpoint }) {
     let array = requestPerSamplingRateMsARRAY
-
+    let count = 0;
     array.forEach((item, index) => {
         setTimeout(() => {
-            runThisNTimes(() => requestThisEndPoint('http://localhost:8000/mockl'), item, true, samplingRateMs)
-        }, samplingRateMs * (index + 1)) // run Nth request every n times
+            count = count + 1;
+            runThisNTimes(() => requestThisEndPoint(endpoint), item, true, samplingRateMs);
+            console.log({ count },);
+        }, samplingRateMs * (index)) // run Nth request every n times
 
     })
 
 }
 
 // # of requests per range-time-unit
-const myArr = [101,
-    90,
-    151,
+const myArr = [
+    10,
+    20,
+    10,
+    121,
+    144,
+    155,
     88,
-    88,
-    81,
-    77,
-    74,
-    78,
+    124,
+    200,
+    300,
+    190,
+    600,
+    20,
+    10,
     100,
     92,
     89,
@@ -1496,4 +1504,15 @@ const myArr = [101,
     94,
 ]
 
-simulation({ samplingRateMs: 60000, requestPerSamplingRateMsARRAY: myArr })
+// simulation({ samplingRateMs: 15000, requestPerSamplingRateMsARRAY: myArr, endpoint: 'http://localhost:8000/mockl' })
+
+
+const endpoints = ['http://localhost:8000/mockl', 'http://localhost:8000/mock2'];
+
+function multipleEndpointLoadTest({ endpoints, simulationFunction }) {
+    endpoints.forEach(endpoint => {
+        simulationFunction({ samplingRateMs: 15000, requestPerSamplingRateMsARRAY: myArr, endpoint: endpoint })
+    })
+}
+
+multipleEndpointLoadTest({ endpoints: endpoints, simulationFunction: simulation })
